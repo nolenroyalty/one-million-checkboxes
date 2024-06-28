710574c3acc879720532c129d1d9149113f0cc36
Nolen <nolen.royalty@gmail.com>
2024-06-28 03:10:38 -0400

    jitter

--- gunicorn_restart.sh
+++ gunicorn_restart.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Function to count running servers
+# jitter
+sleep $((RANDOM % 300)) 
+
 count_running_servers() {
     ps aux | grep gunicorn | grep -oP 'bind \K0\.0\.0\.0:\d+' | sort | uniq | wc -l
 }
 
-# Main logic
 running_servers=$(count_running_servers)
 
 if [ $running_servers -lt 3 ]; then
     echo "Less than 3 servers running. Restarting all servers..."
     
-    # Stop the service
     sudo systemctl stop one-million-checkboxes.service
     
-    # Wait for 10 seconds
     sleep 10
     
-    # Start the service
     sudo systemctl start one-million-checkboxes.service
     
     echo "Servers restarted."
