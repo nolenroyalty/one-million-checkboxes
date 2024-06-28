3cd6927c661d2391af3439210b299002684ecc33
Nolen <nolen.royalty@gmail.com>
2024-06-27 21:50:08 -0400

    reduce worker processes by 1

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -2,7 +2,7 @@
 
 BASE_PORT=5001
 
-for i in 0 1 2 3 4
+for i in 0 1 2 3
 do
     PORT=$((BASE_PORT + i))
     gunicorn --worker-class eventlet --workers 1 --threads 3 --bind 0.0.0.0:$PORT server:app &
